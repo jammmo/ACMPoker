137a80b0dcbcaa78f2fe3535be45505894684dd7
Jamie Moschella <40072741+jammmo@users.noreply.github.com>
2019-03-13 19:34:38 -0400

    Add files via upload

--- pokersim.py
+++ pokersim.py
@@ -1,15 +1,18 @@
 from random import shuffle
 
-pot = 0
-minimum = 0
-
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-    
+
+    def __lt__(self, other):
+        return self.rank < other.rank
+
+    def __eq__(self, other):
+        return self.rank == other.rank and self.suit == other.suit
+
     def __repr__(self):
-        return self.rank + '-' + self.suit
+        return str(self.rank) + '-' + self.suit
 
 class Player:
     def __init__(self, cards, chips):
@@ -31,10 +34,61 @@ class Player:
         self.chips -= added
         pot += added
 
-ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+handorder = ['Royal Flush', 'Straight Flush', 'Four of a Kind', 'Full House', 'Flush', 'Straight', 'Three of a Kind', 'Two Pair', 'One Pair', 'High Card']
+ranks = list(range(2, 15))
 suits = ['D', 'S', 'C', 'H']
+
+pot = 0
+minimum = 0
 deck = [Card(r, s) for r in ranks for s in suits]
 shuffle(deck)
+
+
+def highestHand(cards):
+    assert(len(cards) == 5)
+    suitset = set(x.suit for x in cards)
+    rankset = set(x.rank for x in cards)
+    if len(suitset) == 1 and rankset == {14, 13, 12, 11, 10}:
+        return 'Royal Flush'
+    elif len(suitset) == 1 and any(rankset == set(range(x, x+5)) for x in range(2, 10)):
+        return 'Straight Flush'
+    elif len(suitset) == 1 and rankset == {14, 2, 3, 4, 5}:   #case where ace is low
+        return 'Straight Flush'
+    elif len(rankset) == 2 and [x.rank for x in cards].count(cards[0].rank) in {1, 4}:
+        return 'Four of a Kind'
+    elif len(rankset) == 2 and [x.rank for x in cards].count(cards[0].rank) in {2, 3}:
+        return 'Full House'
+    elif len(suitset) == 1:
+        return 'Flush'
+    elif any(rankset == set(range(x, x+5)) for x in range(2, 10)):
+        return 'Straight'
+    elif rankset == {14, 2, 3, 4, 5}:   #case where ace is low
+        return 'Straight'
+    elif any([x.rank for x in cards].count(y) == 3 for y in rankset):
+        return 'Three of a Kind'
+    elif len(rankset) == 3:
+        return 'Two Pair'
+    elif len(rankset) == 4:
+        return 'One Pair'
+    else:
+        return 'High Card'
+
+def hands(cards):
+    assert(len(cards) == 7)
+    foundhands = {}
+    for x in cards:
+        for y in cards:
+            copied = cards.copy()
+            if x != y:
+                copied.remove(x)
+                copied.remove(y)
+                newhand = highestHand(copied)
+                if newhand in foundhands:
+                    foundhands[newhand].append(cards)
+                else:
+                    foundhands[highestHand(copied)] = [cards]
+    return foundhands
 
 def game(numplayers, startingchips, bigblind):
     #dealing
